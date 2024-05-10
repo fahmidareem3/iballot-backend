3dc5b14ff17754313a119178b2237a790ef03ec7
fahmidareem3 <fahmidareem3@gmail.com>
2024-05-11 01:05:09 +0600

    feat(election): Get all election by organization

--- routes/election.py
+++ routes/election.py
@@ -1,7 +1,8 @@
+from typing import List
 from fastapi import APIRouter, HTTPException, Body, status
 
 from models.election import Election
-from schemas.election import CreateElectionModel
+from schemas.election import CreateElectionModel,ElectionResponse
 
 router = APIRouter()
 
@@ -10,3 +11,11 @@ async def create_election(election_data: CreateElectionModel = Body(...)):
     election = Election(**election_data.dict())
     await election.insert()
     return election
+
+
+@router.get("/by-organization/{organization_id}", response_model=List[ElectionResponse])
+async def get_elections_by_organization(organization_id: str):
+    elections = await Election.find(Election.organization_id == organization_id).to_list()
+    if not elections:
+        raise HTTPException(status_code=404, detail="No elections found for this organization")
+    return elections
--- schemas/election.py
+++ schemas/election.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import List
 from datetime import datetime
+from beanie import PydanticObjectId
 class Candidate(BaseModel):
     user_id: str
     name: str
@@ -13,3 +14,15 @@ class CreateElectionModel(BaseModel):
     start_time: datetime = Field(..., example="2024-01-01T00:00:00Z")
     end_time: datetime = Field(..., example="2024-12-31T23:59:59Z")
     candidates: List[Candidate] = []  
+
+class ElectionResponse(BaseModel):
+    id: PydanticObjectId
+    name: str
+    organization_id: str
+    organization_name: str
+    start_time: datetime
+    end_time: datetime
+    candidates: List[Candidate]
+
+    class Config:
+        orm_mode = True
